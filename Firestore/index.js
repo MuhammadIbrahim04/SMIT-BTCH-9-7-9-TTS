29da9133c34ba5ed8ef46ed37425282f963b2e36
MuhammadIbrahim04 <121630494+MuhammadIbrahim04@users.noreply.github.com>
2023-08-07 10:09:55 -0700

    firestorefinal07-08-23
    firestorefinal07-08-23

--- Firestore/index.js
+++ Firestore/index.js
@@ -6,25 +6,35 @@ import {
 
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import {
-    getDatabase,
-    ref,
-    set
+    doc,
+    setDoc,
+    getFirestore,
+    getDoc,
 }
-    from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+    from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+// import {
+//     getDatabase,
+//     ref,
+//     set,
+//     onValue
+// }
+//     from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA5gwYB3t4WdP8QSNET5-UhOBshItKMAB4",
-    authDomain: "fir-02-08-23.firebaseapp.com",
-    projectId: "fir-02-08-23",
-    storageBucket: "fir-02-08-23.appspot.com",
-    messagingSenderId: "481846330704",
-    appId: "1:481846330704:web:424aadb6d078377ae59855",
-    measurementId: "G-1XMECZC6BF"
+    apiKey: "AIzaSyAWqiJEPwRdARUpdxPE4Rm71O8niWzIYUs",
+    authDomain: "testfirestore-d8e7f.firebaseapp.com",
+    projectId: "testfirestore-d8e7f",
+    storageBucket: "testfirestore-d8e7f.appspot.com",
+    messagingSenderId: "158872388326",
+    appId: "1:158872388326:web:eee9b2361ca8108ece7127",
+    measurementId: "G-H6Z0W7F57S"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getDatabase();
+// const db = getDatabase();
+const db = getFirestore();
 
 let register_btn = document.getElementById('register_btn');
 
@@ -32,15 +42,23 @@ register_btn.addEventListener('click', function () {
     let email = document.getElementById('register_email');
     let password = document.getElementById('register_password');
     createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then((userCredential) => {
+
+        .then(async (userCredential) => {
             // Signed in 
             const user = userCredential.user;
+            // Add a new document in collection "cities"
+            await setDoc(doc(db, "Users", user.uid), {
+                email: email.value,
+                password: password.value,
 
-            set(ref(db,'users/${user.uId}'),{
-                email: email.vaule,
-                password: password.vaule,
+            });
 
-            })
+
+            // set(ref(db, `users/${user.uid}`), {
+            //     email: email.value,
+            //     password: password.value,
+
+            // });
             // ...
         })
         .catch((error) => {
@@ -61,10 +79,22 @@ login_btn.addEventListener('click', function () {
     let login_password = document.getElementById('login_password')
 
     signInWithEmailAndPassword(auth, login_email.value, login_password.value)
-        .then((userCredential) => {
+        .then(async (userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            console.log("user==>", user)
+
+            const docRef = doc(db, "Users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                console.log("Document data:", docSnap.data());
+            } else {
+                // docSnap.data() will be undefined in this case
+                console.log("No such document!");
+            }
+            // onValue(ref(db, `users/${user.uid}`), (data) => {
+            //     console.log("==>data", data.val());
+            // });
             // ...
         })
         .catch((error) => {
@@ -77,11 +107,3 @@ login_btn.addEventListener('click', function () {
 
 
 
-function writeUserData(userId, name, email, imageUrl) {
-    const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-        username: name,
-        email: email,
-        profile_picture: imageUrl
-    });
-}
